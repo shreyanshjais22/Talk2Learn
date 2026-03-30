8b732254bb045872baa981eea3572dd422dc5c3c
shreyansh jaiswal <146716527+shreyanshjais22@users.noreply.github.com>
2026-03-30 19:35:11 +0530

    Update BASE_URL to include '/api' endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://talk2learn-2.onrender.com";
+const BASE_URL = "https://talk2learn-2.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
