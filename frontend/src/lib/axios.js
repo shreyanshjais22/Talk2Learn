d26392901fa9670130d07e704dd569a017649dfb
shreyansh jaiswal <146716527+shreyanshjais22@users.noreply.github.com>
2026-03-30 19:01:21 +0530

    Update BASE_URL for axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://talk2learn.onrender.com/api";
+const BASE_URL = "https://talk2learn-2.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
